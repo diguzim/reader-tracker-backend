ac762ec72c12f3c58382b42c2e7339e500343b9b
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2021-02-21 12:51:35 -0300

    Setting up RSpec for testing, adding a simple test for Book model

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:author) }
+  it { should validate_numericality_of(:pages).only_integer }
+  it { should validate_numericality_of(:relevance) }
+end

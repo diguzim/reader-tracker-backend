8764aef19db7d02d98ad76eb3eda03321ab61760
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2021-02-23 10:10:27 -0300

    Implementing GET single reading

--- app/controllers/api/v1/readings_controller.rb
+++ app/controllers/api/v1/readings_controller.rb
@@ -6,10 +6,11 @@ class Api::V1::ReadingsController < ApplicationController
     json_response(@book.readings)
   end
 
-  def create
-  end
-
   def show
+    json_response(@reading)
+  end
+  
+  def create
   end
 
   def update
@@ -26,5 +27,9 @@ class Api::V1::ReadingsController < ApplicationController
 
   def set_book
     @book = Book.find(params[:book_id])
+  end
+
+  def set_reading
+    @reading = @book.readings.find_by!(id: params[:id]) if @book
   end
 end
--- spec/requests/readings_spec.rb
+++ spec/requests/readings_spec.rb
@@ -18,5 +18,43 @@ RSpec.describe 'Readings API', type: :request do
         expect(json.size).to eq(20)
       end
     end
+
+    context 'when book doesn\'t exists' do
+      let(:book_id) { 0 }
+      
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Book/)
+      end
+    end
+  end
+
+  describe('GET /books/:book_id/readings/:reading_id') do
+    before { get "/api/v1/books/#{book_id}/readings/#{reading_id}" }
+
+    context 'when reading exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the reading' do
+        expect(json['id']).to eq(reading_id)
+      end
+    end
+
+    context 'when reading does not exists' do
+      let(:reading_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+      
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Reading/)
+      end
+    end
   end
 end
